a3c7a20ff14fdf84343367cdd7b1736015df3af2
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2019-03-01 23:01:52 -0500

    import/install pynput creating a key listening with 2 functions to record key press and key release

--- logger.py
+++ logger.py
@@ -5,10 +5,13 @@ from pynput.keyboard import Key, Listener
 
 
 def on_press(key):
-    pass
+    #log into console what keys are pressed
+    print("(0) pressed".format(key))
 
 def on_release(key):
-    pass
+    #break out fo the program when the Escape key is pressed
+    if key == Key.esc:
+        return False
 
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
