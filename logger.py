248b340874d95b7d26c342714af72858a5844e60
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2019-03-01 22:59:55 -0500

    import/install pynput creating a key listening with 2 functions to record key press and key release

--- logger.py
+++ logger.py
@@ -1,3 +1,15 @@
+# run cmd -m pip install pynput
 import pynput
 
 from pynput.keyboard import Key, Listener
+
+
+def on_press(key):
+    pass
+
+def on_release(key):
+    pass
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
